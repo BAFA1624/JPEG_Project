4e416ab12c55401457bbdef7fb541580d5cf04eb
Ben Andrews <benafandrews@gmail.com>
2025-06-01 14:47:26 +0100

    Added empty class defs for critical chunks.

--- include/png/png_chunk.hpp
+++ include/png/png_chunk.hpp
@@ -11,17 +11,27 @@ namespace PNG
 class PngChunk
 {
     public:
+    // The base class should not be constructible or assignable
+    // Constructors
     PngChunk() = delete;
+    PngChunk( const PngChunk & other ) = delete;
+    PngChunk( PngChunk && other ) = delete;
+    // Assignment
+    PngChunk & operator=( const PngChunk & rhs ) = delete;
+    PngChunk & operator=( PngChunk && rhs ) = delete;
     virtual ~PngChunk() = default;
 
     // Unique ptr type to the PngChunk base class
     using const_base_pointer_t = const std::shared_ptr<const PngChunk>;
     using base_pointer_t = const std::shared_ptr<PngChunk>;
 
+    // Returns the png_chunk_t for the current chunk
     [[nodiscard]] constexpr auto get_chunk_type() const noexcept {
         return m_chunk_type;
     }
 
+    // Returns either a modifiable or const pointer to the previous chunk in the
+    // chain.
     [[nodiscard]] base_pointer_t previous_chunk() { return m_previous_chunk; }
     [[nodiscard]] const_base_pointer_t previous_chunk() const {
         return m_previous_chunk;
@@ -55,6 +65,27 @@ class PngChunk
 };
 
 class PngChunkIHDR : public PngChunk
-{};
+{
+    private:
+    static constexpr auto m_chunk_type{ png_chunk_t::IHDR };
+};
+
+class PngChunkPLTE : public PngChunk
+{
+    private:
+    static constexpr auto m_chunk_type{ png_chunk_t::PLTE };
+};
+
+class PngChunkIDAT : public PngChunk
+{
+    private:
+    static constexpr auto m_chunk_type{ png_chunk_t::IDAT };
+};
+
+class PngChunkIEND : public PngChunk
+{
+    private:
+    static constexpr auto m_chunk_type{ png_chunk_t::IEND };
+};
 
 } // namespace PNG
