2ebab2bd9483b6291b944a79ff7fd0bb271cf4a9
Ben Andrews <ben.andrews@arm.com>
2025-09-03 13:46:09 +0100

    Added forwarding of passed functions.

--- tests/include/test_interface.hpp
+++ tests/include/test_interface.hpp
@@ -6,20 +6,24 @@ namespace TEST_INTERFACE
 {
 
 template <typename T, typename Output, typename... Inputs>
-concept ValidTestCallable =
-    requires( T && f, const Output & output, Inputs &&... inputs ) {
-        { f( std::forward<Inputs>( inputs )... ) } -> std::same_as<Output>;
-        {
-            output == f( std::forward<Inputs>( inputs )... )
-        } -> std::convertible_to<bool>;
-    };
+concept ValidTestCallable = requires( T && func, const Output & output,
+                                      Inputs &&... inputs ) {
+    {
+        std::forward<T>( func )( std::forward<Inputs>( inputs )... )
+    } -> std::same_as<Output>;
+    {
+        output == std::forward<T>( func )( std::forward<Inputs>( inputs )... )
+    } -> std::convertible_to<bool>;
+};
 
 template <typename Func, typename Output, typename... Inputs>
     requires ValidTestCallable<Func, Output, Inputs...>
 constexpr bool
 test_function( Func && function, const Output expected_output,
                Inputs &&... inputs ) {
-    return expected_output == std::forward<Func>( function )( inputs... );
+    return expected_output
+           == std::forward<Func>( function )(
+               std::forward<Inputs>( inputs )... );
 }
 
 } // namespace TEST_INTERFACE
