e2de48d34ae8cfbfa4507908b092c6ae14562b09
Ben Andrews <benafandrews@gmail.com>
2025-09-05 02:45:13 +0100

    Removed unused testing code.

--- tests/png/png_types_test.cpp
+++ tests/png/png_types_test.cpp
@@ -6,27 +6,8 @@ namespace PNG
 {
 
 namespace PNG_TYPES
-{
+{} // namespace PNG_TYPES
 
-void
-tmp( const TestError auto error ) {
-    std::cout << error.what() << std::endl;
-}
-
-void
-test_func( const Test<double, std::runtime_error, double, double> auto tmp ) {}
-
-// Test: png_chunk_t
-
-// Test: PNGChunk
-/* PNGChunk::PNGChunk():*/
-/* PNGChunk::PNGChunk( const std::uint32_t _len, const png_chunk_t _type,
-                       std::unique_ptr<char *> _source_ptr,
-                       const std::bitset<32> & _crc ):*/
-/* PNGChunk::PNGChunk( const std::uint32_t _len, const png_chunk_t _type,
-                       const char * const      _source_ptr,
-                       const std::bitset<32> & _crc ):*/
-} // namespace PNG_TYPES
 } // namespace PNG
 
 int
