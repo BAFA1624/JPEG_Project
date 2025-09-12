d337457fc8cd5f8913311f67c4fc7bf045ea012a
Ben Andrews <benafandrews@gmail.com>
2025-09-12 03:38:55 +0100

    Empty template for png chunk payload unit tests.

--- tests/include/png/png_chunk_payload_test.hpp
+++ tests/include/png/png_chunk_payload_test.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
 #include "png/png_chunk_payload.hpp"
+
+namespace PNG
+{
+
+bool test_png_chunk_payload_base();
+
+const auto test_functions = std::vector{ test_png_chunk_payload_base };
+
+} // namespace PNG
--- tests/png/png_chunk_payload_test.cpp
+++ tests/png/png_chunk_payload_test.cpp
@@ -1,1 +1,23 @@
 #include "png/png_chunk_payload_test.hpp"
+
+namespace PNG
+{
+
+bool
+test_png_chunk_payload_base() {
+    return true;
+}
+
+} // namespace PNG
+
+int
+png_chunk_payload_test( [[maybe_unused]] int     argc,
+                        [[maybe_unused]] char ** argv ) {
+    std::size_t test_passes{ 0 };
+
+    for ( const auto & func : PNG::test_functions ) {
+        test_passes += ( func() ? 1 : 0 );
+    }
+
+    return static_cast<int>( PNG::test_functions.size() - test_passes );
+}
