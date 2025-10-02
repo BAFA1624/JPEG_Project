1f4858d6c2c0e5a13a42c7115a96165c204b922d
Ben Andrews <ben.andrews@arm.com>
2025-10-02 15:06:10 +0100

    Added PayloadTestFixture class.

--- tests/include/png/png_chunk_payload_test.hpp
+++ tests/include/png/png_chunk_payload_test.hpp
@@ -3,39 +3,89 @@
 #include "png/png_chunk_payload.hpp"
 #include "test_interface.hpp"
 
+#include <format>
+#include <iostream>
+#include <string_view>
+
 namespace TEST_INTERFACE
-{
-template <typename Payload, bool ExpectedResult, typename... Args>
-    requires std::constructible_from<Payload, Args...>
-constexpr bool
-test_payload( const Args &&... args ) {
-    const Payload payload{ std::forward<const Args>( args )... };
-    return payload.isValid() == ExpectedResult;
-}
-template <typename Payload, typename... Args>
-constexpr bool
-test_payload( const bool expected_result, const Args &&... args ) {
-    const Payload payload{ std::forward<const Args>( args )... };
-    return payload.isValid() == expected_result;
-}
-} // namespace TEST_INTERFACE
+{}
 
 namespace PNG
 {
+
+namespace
+{
+
+template <typename Payload>
+    requires std::is_base_of_v<PngChunkPayloadBase, Payload>
+class PayloadTestFixture : public TEST_INTERFACE::TestFixture
+{
+    private:
+    std::vector<std::pair<std::string_view, Payload>> test_targets;
+
+    public:
+    PayloadTestFixture( const std::string_view id,
+                        std::ostream &         out = std::cerr ) :
+        TEST_INTERFACE::TestFixture( id, out ) {}
+    ~PayloadTestFixture() = default;
+    PayloadTestFixture( const PayloadTestFixture & ) = delete;
+    PayloadTestFixture & operator=( const PayloadTestFixture & ) = delete;
+    PayloadTestFixture( PayloadTestFixture && ) = delete;
+    PayloadTestFixture & operator=( PayloadTestFixture && ) = delete;
+
+    template <typename... Args>
+        requires std::constructible_from<Payload, Args...>
+    constexpr void addPayload( const std::string_view payload_name,
+                               Args &&... args ) {
+        test_targets.emplace_back( std::pair{
+            payload_name, Payload{ std::forward<Args>( args )... } } );
+    }
+
+    [[nodiscard]] constexpr bool
+    test_valid( const std::vector<bool> & expected_outputs ) const noexcept {
+        assert( expected_outputs.size() == test_targets.size() );
+
+        bool overall_result{ true };
+        for ( const auto & [payload_data, expected_output] :
+              std::views::zip( test_targets, expected_outputs ) ) {
+            const auto & [payload_name, payload] = payload_data;
+
+            *this << std::format( " Running {}... ", payload_name );
+
+            constexpr bool result{ payload.isValid == expected_output };
+
+            *this << std::format( " Done" );
+        }
+        *this << std::flush;
+
+        return overall_result;
+    }
+};
+
+template <typename Payload, typename... Args>
+    requires std::constructible_from<Payload, Args...>
+constexpr bool
+test_payload_valid( const Args &&... args ) {
+    const Payload payload{ std::forward<const Args>( args )... };
+    return payload.isValid();
+}
+} // namespace
+
 bool test_png_chunk_payload_base();
 
 namespace IHDR
 {
 
 // clang-format off
-/* |-----------------------------------------------------------------------------------------------------------------------|
- * | Expression | ExpectedValid | Expression && ExpectedValid | Expression || ExpectedValid | Expression || !ExpectedValid |
- * |------------|---------------|-----------------------------|-----------------------------|------------------------------|
- * |    true    |   true        |           true              |           true              |           true               |
- * |    true    |   false       |           false             |           true              |           true               |
- * |    false   |   true        |           false             |           true              |           false              |
- * |    false   |   false       |           false             |           false             |           true               |
- * |-----------------------------------------------------------------------------------------------------------------------|
+//
+/* |----------------------------||------------------------------------------------------------------------------------------------------------------------|
+ * | Expression | ExpectedValid || Expression && ExpectedValid | Expression || ExpectedValid | Expression || !ExpectedValid | Expression == ExpectedValid |
+ * |------------|---------------||-----------------------------|-----------------------------|------------------------------|-----------------------------|
+ * |    true    |   true        ||           true              |           true              |           true               |           true              |
+ * |    true    |   false       ||           false             |           true              |           true               |           false             |
+ * |    false   |   true        ||           false             |           true              |           false              |           true              |
+ * |    false   |   false       ||           false             |           false             |           true               |           false             |
+ * |----------------------------||------------------------------------------------------------------------------------------------------------------------|
  */
 // clang-format on
 template <bool Expression, bool ExpectedValid>
