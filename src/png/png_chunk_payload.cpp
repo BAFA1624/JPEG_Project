37e714e006ae295f00ed6f486a6cbfe81cda5ff9
Ben Andrews <ben.andrews@arm.com>
2025-11-19 17:38:42 +0000

    span_to_integer now uses IntOrEnum concept.
    - Altered span_to_integer to accept both integral types & enumerations
      in it's template parameters.
    - Altered usage sites of span_to_integer in png_chunk_payload.cpp as a
      result.

--- src/png/png_chunk_payload.cpp
+++ src/png/png_chunk_payload.cpp
@@ -31,40 +31,35 @@ IhdrChunkPayload::IhdrChunkPayload(
 
     constexpr std::size_t bit_depth_offset{ height_offset
                                             + sizeof( bit_depth ) };
-    bit_depth = static_cast<BitDepth>(
+    bit_depth =
         span_to_integer<BitDepth, std::endian::big, std::endian::native>(
-            raw_data.subspan( height_offset, sizeof( bit_depth ) ) ) );
+            raw_data.subspan( height_offset, sizeof( bit_depth ) ) );
 
     constexpr std::size_t colour_type_offset{ bit_depth_offset
                                               + sizeof( colour_type ) };
-    colour_type = static_cast<ColourType>(
-        span_to_integer<std::underlying_type_t<ColourType>,
-                        std::endian::big,
-                        std::endian::native>(
-            raw_data.subspan( bit_depth_offset, sizeof( colour_type ) ) ) );
+    colour_type =
+        span_to_integer<ColourType, std::endian::big, std::endian::native>(
+            raw_data.subspan( bit_depth_offset, sizeof( colour_type ) ) );
 
     constexpr std::size_t compression_method_offset{
         colour_type_offset + sizeof( compression_method )
     };
-    compression_method = static_cast<CompressionMethod>(
-        span_to_integer<std::underlying_type_t<CompressionMethod>,
-                        std::endian::big,
-                        std::endian::native>( raw_data.subspan(
-            colour_type_offset, sizeof( compression_method ) ) ) );
+    compression_method = span_to_integer<CompressionMethod,
+                                         std::endian::big,
+                                         std::endian::native>(
+        raw_data.subspan( colour_type_offset, sizeof( compression_method ) ) );
 
     constexpr std::size_t filter_method_offset{ compression_method_offset
                                                 + sizeof( filter_method ) };
-    filter_method = static_cast<FilterMethod>(
-        span_to_integer<std::underlying_type_t<FilterMethod>,
-                        std::endian::big,
-                        std::endian::native>( raw_data.subspan(
-            compression_method_offset, sizeof( filter_method ) ) ) );
+    filter_method =
+        span_to_integer<FilterMethod, std::endian::big, std::endian::native>(
+            raw_data.subspan( compression_method_offset,
+                              sizeof( filter_method ) ) );
 
-    interlace_method = static_cast<InterlaceMethod>(
-        span_to_integer<std::underlying_type_t<InterlaceMethod>,
-                        std::endian::big,
-                        std::endian::native>( raw_data.subspan(
-            filter_method_offset, sizeof( interlace_method ) ) ) );
+    interlace_method =
+        span_to_integer<InterlaceMethod, std::endian::big, std::endian::native>(
+            raw_data.subspan( filter_method_offset,
+                              sizeof( interlace_method ) ) );
 }
 
 constexpr IhdrChunkPayload::IhdrChunkPayload(
